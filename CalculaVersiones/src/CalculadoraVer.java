84979953b83bb2c0a305a2d7cc2ced2720ee7f1e
DavidBlan7 <david1991blanco@gmail.com>
2020-03-23 18:45:59 +0100

    Metodo resta

--- CalculaVersiones/src/CalculadoraVer.java
+++ CalculaVersiones/src/CalculadoraVer.java
@@ -9,4 +9,14 @@ public class CalculadoraVer {
 		this.num2 = num2;
 	}
 	
+	
+	public int sumar(int num1, int num2) {
+		int suma = num1 + num2;
+		return suma;
+	}
+	
+	public int restar(int num1, int num2) {
+		int resta = num1 - num2;
+		return resta;
+	}
 }
